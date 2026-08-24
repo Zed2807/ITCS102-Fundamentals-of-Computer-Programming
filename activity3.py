66666bab9d9f25ce6389498279ba0cc906d78d01
Zed2807 <zedrickumbrete28@gmail.com>
2026-08-24 21:49:22 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,58 @@
+#using variables
+
+#int
+age = 19
+class_size = 47
+fav_number = 1
+Walang_Pera = 0
+
+#float
+barya_ko = 0.25
+ilang_oras_naubos = 0.2
+temp_ko_noong_naubos_ang_pera_ko = 30.2
+gumaling = 1.2
+
+#string
+username = "pikachu"
+password = "si_jazzy_ay_sapat_na"
+favorite_character = "Iron_Man"
+love_language = "sleep"
+
+#bool
+is_zedrick_pogi = True
+is_zedrick_gay = False
+is_zedrick_single = False
+is_zedrick_taken = True
+
+
+print("ilan taon na ako",age)
+
+print("fav number ko",fav_number)
+
+print("ilan kami sa room",class_size)
+
+print("magkano pera mo",Walang_Pera)
+
+print("barya mo",barya_ko)
+
+print("ilang oras mo naubos ang pera",ilang_oras_naubos)
+
+print("temperatura ko noong naubos pera ko",temp_ko_noong_naubos_ang_pera_ko)
+
+print("ilang weeks ako gumaling",gumaling)
+
+print("username",username)
+
+print("password",password)
+
+print("favorite character ko si", favorite_character)
+
+print("ang love language ko is",love_language)
+
+print("gwapo ba ako",is_zedrick_pogi)
+
+print("bading ba ako",is_zedrick_single)
+
+print("tama bang single ako",is_zedrick_single)
+
+print("taken na raw ako",is_zedrick_taken)
