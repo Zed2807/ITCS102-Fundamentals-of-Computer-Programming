b32f286067bb6d8f5d363edf9b6abf005e3c1d8b
Zed2807 <zedrickumbrete28@gmail.com>
2026-08-26 01:48:07 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,13 @@
+#escape sequence
+# \n - new line
+# \t - tab
+# \r - carriage return/ deprecated/obsolete
+# \b - backspace
+# \\ - insert slash
+# \" - insert double quotation
+
+phrase1 = "\"there'\bs \\a hole inside \nthe \t\thole"
+phrase2 = "love \"is al\t\tways love"
+
+print(phrase1)
+print(phrase2)
