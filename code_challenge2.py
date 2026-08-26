e36939bc691b67a8ef20c41ed3ec91d7ba3f6f4b
Zed2807 <zedrickumbrete28@gmail.com>
2026-08-27 00:18:48 +0800

    Add files via upload

---
+++ code_challenge2.py
@@ -0,0 +1,50 @@
+a = 1000
+b = 500
+c = 200
+d = 100
+e = 50
+f = 20
+g = 10
+h = 5
+i = 1
+
+amount = 67894
+print("amount to deposit -->",amount)
+total = amount // a
+amount = amount % a
+
+total2 = amount // b
+amount = amount % b
+
+total3 = amount // c
+amount = amount % c
+
+total4 = amount // d
+amount = amount % d
+
+total5 = amount // e
+amount = amount % e
+
+total6 = amount // f
+amount = amount % f
+
+total7 = amount // g
+amount = amount % g
+
+total8 = amount // h
+amount = amount % h
+
+total9 = amount // i
+amount = amount % i
+
+
+print("1000:",total)
+print("500:",total2)
+print("200:",total3)
+print("100:",total4)
+print("50:",total5)
+print("20:",total6)
+print("10:",total7)
+print("5:",total8)
+print("1:",total9)
+
