4b1ef186e5b0fdf8823d546e1f6fe14348561516
Zed2807 <zedrickumbrete28@gmail.com>
2026-09-02 06:07:07 -0700

    Add files via upload

--- code_challenge2.py
+++ code_challenge2.py
@@ -8,8 +8,11 @@ g = 10
 h = 5
 i = 1
 
-amount = 67894
-print("amount to deposit -->",amount)
+amount = eval(input("\nEnter Money to DEPOSIT ---->> ")) # imt(), eval(), type()
+#print(type(amount)
+print()
+print(" =================================== Philippines Bank Denomination ============================= ")
+print("\nAMOUNT TO DEPOSIT -->",amount)
 total = amount // a
 amount = amount % a
 
@@ -38,13 +41,14 @@ total9 = amount // i
 amount = amount % i
 
 
-print("1000:",total)
-print("500:",total2)
-print("200:",total3)
-print("100:",total4)
-print("50:",total5)
-print("20:",total6)
-print("10:",total7)
-print("5:",total8)
-print("1:",total9)
+print("\t\t1000:",total)
+print("\t\t500:",total2)
+print("\t\t200:",total3)
+print("\t\t100:",total4)
+print("\t\t50:",total5)
+print("\t\t20:",total6)
+print("\t\t10:",total7)
+print("\t\t5:",total8)
+print("\t\t1:",total9)
 
+print(" ========================================== END OF TRACK ====================================== ")
