e3842671f5d5c8995ad9e70b393460a51733bf8c
Zed2807 <zedrickumbrete28@gmail.com>
2026-09-05 02:28:55 -0700

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,30 @@
+hobbies = ""
+
+U = input("What are your hobbies?")
+hobbies += U + ","
+
+m = input("What are your hobbies?")
+hobbies += m + ","
+
+b = input("What are your hobbies?")
+hobbies += b + ","
+
+r = input("What are your hobbies?")
+hobbies += r + ","
+
+e = input("What are your hobbies?")
+hobbies += e + ","
+
+t = input("What are your hobbies?")
+hobbies += t + ","
+
+e = input("What are your hobbies?")
+hobbies += e + ","
+
+z = input("What are your hobbies?")
+hobbies += z + ","
+
+s = input("What are your hobbies?")
+hobbies += s + ","
+
+print("Your hobbies are {", hobbies ,"}")
