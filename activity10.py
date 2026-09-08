fbd656a0ebbd741f0a91995af3a1e8f81327b2f4
Zed2807 <zedrickumbrete28@gmail.com>
2026-09-08 06:05:19 -0700

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,12 @@
+# basic if else program
+
+username = "Jazzymaganda"
+password = "siyalangtlga"
+
+u = input("input USERNAME ===>")
+p = input("input PASSWORD ===>")
+
+if u == username and p == password :
+	print("username and password correct")
+else:
+	print("DENIED")
